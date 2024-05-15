81dc83f92211e21bd735847c1d46f4a52893f0c5
justin2303 <justin.ypc@gmail.com>
2024-05-14 19:26:33 -0700

    made mazes guaranteed to not be trivial
    used manhattan distance to quantify distance from start to end, ensured that maze followed min distance, also tried implementing a maze generator based on a priority queue, results sucked.

--- backend/api/routes/maze.py
+++ backend/api/routes/maze.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from api import app
 import random
+import queue
 class Coords:
     def __init__(self, x, y):
         self.X = x
@@ -12,6 +13,15 @@ class Coords:
         if isinstance(other, Coords):
             return self.X == other.X and self.Y == other.Y
         return False
+    def __lt__(self, other):
+        if isinstance(other, Coords):
+            return self.X == other.X and self.Y == other.Y
+        return False
+    def __gt__(self, other):
+        if isinstance(other, Coords):
+            return self.X == other.X and self.Y == other.Y
+        return False
+    
 
 @app.route("/api/test_maze_solution", methods=["GET"])
 def get_test_maze_solution():
@@ -31,6 +41,14 @@ def get_test_maze_solution():
 @app.route("/api/make_maze_sparse/<int:dim1>/<int:dim2>", methods=["GET"])
 def make_maze_sparse(dim1, dim2):
     #for now ignore dim1dim2
+    maze,distance = make_rand_maze()
+    while(distance <10):
+        maze,distance = make_rand_maze()
+    print("distance: ", distance)
+    return jsonify(maze)
+
+def make_rand_maze():
+#for now ignore dim1dim2
     maze = []
     for i in range(10):
         row = []
@@ -41,13 +59,16 @@ def make_maze_sparse(dim1, dim2):
     random_start =  Coords(random.randint(0, 9),random.randint(0, 9))
     maze[random_start.Y][random_start.X]='X'
     visited =[]
-    spaces = 60
-    recursive_helper(maze,visited,random_start, spaces)
+    spaces = 80
+    print("rand start: ", random_start)
+    maze, current =recursive_helper(maze,visited,random_start, spaces)
     #coord = Coords(1,1)
     #get_neighbors(maze,coord)
     #print(Coords(1,1)==Coords(1,2))
     #print(Coords(1,1)==Coords(1,1))
-    return jsonify(maze)
+    print("current: ", current)
+    print("distance: ",get_dist(random_start, current))
+    return (maze,get_dist(random_start, current))
 
 def get_neighbors(maze, coord):
     neighbors=[]
@@ -63,48 +84,76 @@ def get_neighbors(maze, coord):
     if coord.Y < len(maze) - 1:
         new_coord = Coords(coord.X , coord.Y + 1)
         neighbors.append(new_coord)
-    print(neighbors)
     return neighbors
 
 def recursive_helper(maze,visited,current, spaces):
     if spaces<=0:
         print("setting endpos")
         maze[current.Y][current.X]='Y'
-        return maze
+        return maze,current
     visited.append(current)
     nexts = get_neighbors(maze, current)
-    print("got here1")
     index = random.randint(0, len(nexts)-1)
     found = False
     count = 0
-    print("got here2")
-    current = nexts[index]
+    next = nexts[index]
     for x in range(0, len(nexts)):
         if(len(visited)==1 or found):
             break
         for y in range(0, len(visited)):
-            if visited[y]==current:
+            if visited[y]==next:
                 count += 1
                 if(count==len(nexts)):
                     print("found loop, ending prematurely..")
-                    maze[current.Y][current.X]='Y'
-                    return maze
+                    maze[next.Y][next.X]='Y'
+                    return maze,next
                 if(index==len(nexts)-1):
                     index=0
                 else:
                     index +=1
-                current = nexts[index]
+                next = nexts[index]
                 break
             if y== len(visited)-1:
                 found = True
     #found an unvisited node
-    maze[current.Y][current.X]='0'
-    print("got here3")
-    recursive_helper(maze,visited,current, spaces-1)
+    maze[next.Y][next.X]='0'
+    return recursive_helper(maze,visited,next, spaces-1)
+def get_dist(start, current):
+    print(start, current)
+    return abs(start.X-current.X) + abs(start.Y-current.Y)
+
+def recursive_opt(maze,visited,current, spaces):
+    print("spaces: ", spaces)
+    if spaces<=0:
+        print("setting endpos")
+        maze[current.Y][current.X]='Y'
+        return maze
+    visited.append(current)
+    nexts = get_neighbors(maze, current)
+    pq=queue.PriorityQueue()
+    for next in nexts:
+        pq.put((get_dist(visited[0], next), next))
+    next_dist, next_node = pq.get()
+    print("self: ",visited[0])
+    print(next_node)
+    found = False
+    while(not found):
+        for x in range(len(visited)):
+            print(x)
+            if visited[x] == next_node:
+                if(pq.empty()):
+                    print("circle found")
+                    maze[current.Y][current.X]='Y'
+                    return
+                next_dist, next_node = pq.get()
+                break
+            if(x== len(visited)-1):
+                found=True
+    #found next node
+    maze[next_node.Y][next_node.X]='0'
+    spaces -= 1
+    recursive_opt(maze,visited,next_node, spaces)
     return maze
-    
-
-
 
             
 
