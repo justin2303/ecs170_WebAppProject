98a3bdf0c8ac011934f073d61896149206b74988
Hai Nguyen <hcnguyen@ucdavis.edu>
2024-05-29 11:48:24 -0700

    added api on frontend to call endpoints on backend pt 2

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,17 +1,18 @@
-
+const serverUrl = 'http://localhost:5000';
 
 function get_res() {
-    fetch("http://localhost:5000/api/test_maze_solution")
+    fetch(`${serverUrl}/api/test_maze_solution`)
     .then(response => {
         console.log(response.json())
     })
 }
+
 function make_sparse() {
     var numRows = document.getElementById("N_rows").value;
     var numCols = document.getElementById("N_Cols").value;
     console.log("numRows:", numRows);
     console.log("numCols:", numCols);
-    const url = `http://localhost:5000/api/make_maze_sparse/${numRows}/${numCols}`;
+    const url = `${serverUrl}/api/make_maze_sparse/${numRows}/${numCols}`;
 
     // Make the GET request to the Flask endpoint
     fetch(url)
@@ -19,18 +20,29 @@ function make_sparse() {
         // Parse the JSON response
         return response.json();
     })
-    .then(data => {
+    .then(async data => {
         // Do something with the JSON data returned from the Flask endpoint
-        drawMaze(data)
+        drawMaze(data.maze, data.start_coords, data.end_coords);
+
+        // Move these elsewhere
+        // This is just for demoing
+        const aStarSolution = await getAStarSolution(data.maze, data.start_coords, data.end_coords);
+        console.log(aStarSolution);
+
+        const dijkstraSolution = await getDijkstraSolution(data.maze, data.start_coords, data.end_coords);
+        console.log(dijkstraSolution);
+
+        const beamSearchSolution = await getBeamSearchSolution(data.maze, data.start_coords, data.end_coords, 20);
+        console.log(beamSearchSolution)
     })
     .catch(error => {
         // Handle any errors that occur during the fetch request
         console.error('Error:', error);
     });
 }
-const counter=0
+
 // Define your maze as a 2D array of 1s and 0s
-function drawMaze(maze) {
+function drawMaze(maze, start_coords, end_coords) {
     
     const canvas = document.createElement("canvas")
     const ctx = canvas.getContext("2d")
@@ -48,23 +60,27 @@ function drawMaze(maze) {
             const y = row * cellSize
 
             // Draw walls for 1s
-            if (maze[row][col] === '1') {
+            if (maze[row][col] === 1) {
                 ctx.fillStyle = wallColor
                 ctx.fillRect(x, y, cellSize, cellSize)
-            }else if(maze[row][col] === 'X'){
-                ctx.fillStyle = "red"
-                ctx.fillRect(x, y, cellSize, cellSize)
-            } 
-            else if(maze[row][col] === 'Y'){
-                ctx.fillStyle = "yellow"
-                ctx.fillRect(x, y, cellSize, cellSize)
-            } 
-            else { // Draw free spaces for 0s
+            } else { // Draw free spaces for 0s
                 ctx.fillStyle = freeSpaceColor
                 ctx.fillRect(x, y, cellSize, cellSize)
             }
         }
     }
+
+    const x1 = start_coords[1] * cellSize;
+    const y1 = start_coords[0] * cellSize;
+    ctx.fillStyle = "red";
+    ctx.fillRect(x1, y1, cellSize, cellSize);
+
+
+    const x2 = end_coords[1] * cellSize;
+    const y2 = end_coords[0] * cellSize;
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(x2, y2, cellSize, cellSize);
+
     const mazeContainer = document.getElementById("mazeContainer")
     mazeContainer.innerHTML = ""//set empty.
     mazeContainer.appendChild(canvas)
@@ -72,4 +88,77 @@ function drawMaze(maze) {
 
 function test_neighbors() {
     
+}
+
+async function getAStarSolution(maze, start_coords, end_coords) {
+    return fetch(`${serverUrl}/api/algorithm/astar`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            maze: maze,
+            start_coords: start_coords,
+            end_coords: end_coords
+        })
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const solution = data.solution;
+        return solution;
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+}
+
+async function getDijkstraSolution(maze, start_coords, end_coords) {
+    return fetch(`${serverUrl}/api/algorithm/dijkstra`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            maze: maze,
+            start_coords: start_coords,
+            end_coords: end_coords
+        })
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const solution = data.solution;
+        return solution;
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+}
+
+async function getBeamSearchSolution(maze, start_coords, end_coords, beam_width = 20) {
+    return fetch(`${serverUrl}/api/algorithm/beam_search`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            maze: maze,
+            start_coords: start_coords,
+            end_coords: end_coords,
+            beam_width: beam_width
+        })
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const solution = data.solution;
+        return solution;
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
 }
